8f3c7c89302c91d3cdfc96154d6a6488fb2af006
phil-krull <pkrull@codingdojo.com>
2018-01-29 11:25:32 -0600

    added js demo

---
+++ js/demo/index.js
@@ -0,0 +1,10 @@
+   // this is for javascript
+        // allows for new ES6 features
+        "use strict;"
+        function printToNumber(number) {
+            for(var i = 0; i <= number; i++) {
+                console.log(i);
+            }
+        }
+
+        printToNumber(10);
